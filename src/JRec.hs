451841c9ea7ce0e7710ca0b43f9ba382e28f2da7
Sridhar Ratnakumar <srid@srid.ca>
2020-07-31 18:07:19 -0400

    Put tests in test/

--- src/JRec.hs
+++ src/JRec.hs
@@ -10,12 +10,6 @@ module JRec
     pattern ExactRecord,
     unField,
     union,
-
-    -- * Tests
-    unit_polymorphic,
-    unit_show,
-    unit_get,
-    unit_set,
   )
 where
 
@@ -33,7 +27,6 @@ import Control.Lens ((&), (^.))
 import JRec.Super ((:=))
 import JRec.Tuple
 import JRec.Field
-import Test.Tasty.HUnit
 import Unsafe.Coerce
 
 ----------------------------------------------------------------------------
@@ -121,51 +114,6 @@ instance {-# OVERLAPPING #-} (R.Set field fields a' ~ fields', R.Set field field
 
 instance {-# OVERLAPPING #-} (R.Set field fields a ~ fields, R.Has field fields a) => GL.HasField' field (Record fields) a where
   field' = coerced @(Record fields) @_ @(R.Rec fields) @_ . R.lens (R.FldProxy @field)
-
-unit_polymorphic :: IO ()
-unit_polymorphic = do
-  (ExactRecord (#u :=: True, #a :=: 5, #b :=: 6) & #u .~ 5)
-    @?= ExactRecord (#u :=: 5, #a :=: 5, #b :=: 6)
-
-----------------------------------------------------------------------------
--- Tests
-----------------------------------------------------------------------------
-
-unit_show :: IO ()
-unit_show = do
-  show (ExactRecord ()) @?= "{}"
-  show (ExactRecord (#foo :=: True)) @?= "{foo = True}"
-  show (ExactRecord (#foo :=: True, #bar :=: 0)) @?= "{foo = True, bar = 0}"
-
-unit_get :: IO ()
-unit_get = do
-  let getA1 :: Record ("a" :=: Int ': rest) -> Int
-      getA1 = (^. #a)
-  let getA2 :: Record ("u" :=: Bool ': "a" :=: Int ': rest) -> Int
-      getA2 = (^. #a)
-  getA1 (ExactRecord (#a :=: 5)) @?= 5
-  getA1 (ExactRecord (#a :=: 5, #b :=: 6)) @?= 5
-  getA2 (ExactRecord (#u :=: True, #a :=: 5)) @?= 5
-  getA2 (ExactRecord (#u :=: True, #a :=: 5, #b :=: 6)) @?= 5
-
-unit_set :: IO ()
-unit_set = do
-  let setA1 ::
-        Record ("a" :=: Int ': rest) ->
-        Record ("a" :=: Int ': rest)
-      setA1 = (#a .~ 8)
-  let setA2 ::
-        Record ("u" :=: Bool ': "a" :=: Int ': rest) ->
-        Record ("u" :=: Bool ': "a" :=: Int ': rest)
-      setA2 = (#a .~ 8)
-  setA1 (ExactRecord (#a :=: 5))
-    @?= (ExactRecord (#a :=: 8))
-  setA1 (ExactRecord (#a :=: 5, #b :=: 6))
-    @?= (ExactRecord (#a :=: 8, #b :=: 6))
-  setA2 (ExactRecord (#u :=: True, #a :=: 5))
-    @?= (ExactRecord (#u :=: True, #a :=: 8))
-  setA2 (ExactRecord (#u :=: True, #a :=: 5, #b :=: 6))
-    @?= (ExactRecord (#u :=: True, #a :=: 8, #b :=: 6))
 
 pattern ExactRecord :: RecTuple tuple fields => tuple -> Record fields
 pattern ExactRecord a <-
--- test/JRecSpec.hs
+++ test/JRecSpec.hs
@@ -1,7 +1,43 @@
 module JRecSpec (spec) where
 
 import Test.Hspec
+import Control.Lens ((&), (^.), (.~))
+import JRec
 
 spec :: Spec
 spec = do 
-  pure ()
+  it "polymorphic" $ do
+    (ExactRecord (#u :=: True, #a :=: 5, #b :=: 6) & #u .~ 5)
+      `shouldBe`  ExactRecord (#u :=: 5, #a :=: 5, #b :=: 6)
+  it "show" $ do
+    show (ExactRecord ()) `shouldBe` "{}"
+    show (ExactRecord (#foo :=: True)) `shouldBe` "{foo = True}"
+    show (ExactRecord (#foo :=: True, #bar :=: 0)) `shouldBe` "{foo = True, bar = 0}"
+  it "get" $ do 
+    let getA1 :: Record ("a" :=: Int ': rest) -> Int
+        getA1 = (^. #a)
+    let getA2 :: Record ("u" :=: Bool ': "a" :=: Int ': rest) -> Int
+        getA2 = (^. #a)
+    getA1 (ExactRecord (#a :=: 5)) `shouldBe` 5
+    getA1 (ExactRecord (#a :=: 5, #b :=: 6)) `shouldBe` 5
+    getA2 (ExactRecord (#u :=: True, #a :=: 5)) `shouldBe` 5
+    getA2 (ExactRecord (#u :=: True, #a :=: 5, #b :=: 6)) `shouldBe` 5
+  it "set" $ do
+    let setA1 ::
+          Record ("a" :=: Int ': rest) ->
+          Record ("a" :=: Int ': rest)
+        setA1 = (#a .~ 8)
+    let setA2 ::
+          Record ("u" :=: Bool ': "a" :=: Int ': rest) ->
+          Record ("u" :=: Bool ': "a" :=: Int ': rest)
+        setA2 = (#a .~ 8)
+    setA1 (ExactRecord (#a :=: 5))
+      `shouldBe` (ExactRecord (#a :=: 8))
+    setA1 (ExactRecord (#a :=: 5, #b :=: 6))
+      `shouldBe` (ExactRecord (#a :=: 8, #b :=: 6))
+    setA2 (ExactRecord (#u :=: True, #a :=: 5))
+      `shouldBe` (ExactRecord (#u :=: True, #a :=: 8))
+    setA2 (ExactRecord (#u :=: True, #a :=: 5, #b :=: 6))
+      `shouldBe` (ExactRecord (#u :=: True, #a :=: 8, #b :=: 6))
+
+
