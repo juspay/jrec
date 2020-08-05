aa02001c22410ad9e46ec5ced99177f4b8e6ee67
Sridhar Ratnakumar <srid@srid.ca>
2020-08-04 21:33:15 -0400

    Add ormolu and HLS

--- default.nix
+++ default.nix
@@ -1,10 +1,9 @@
 let 
-  nixpkgsRev = "0f5ce2fac0c726036ca69a5524c59a49e2973dd4";
-  nixpkgsSha = "0nkk492aa7pr0d30vv1aw192wc16wpa1j02925pldc09s9m9i0r3";
-  pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-    sha256 = nixpkgsSha;
-  }) {};
+  nixpkgsSrc = builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/76f2e271a2ef.tar.gz";
+    sha256 = "1r4da3kyghqq7vzya5yb295jpnha1fgp755bi6wda4fmhyaynnsp";
+  };
+  pkgs = import nixpkgsSrc {};
   gitignoreSrc = pkgs.fetchFromGitHub { 
     owner = "hercules-ci";
     repo = "gitignore";
@@ -21,6 +20,8 @@ in
     modifier = drv:
       pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
         [ cabal-install
+          ormolu
+          haskell-language-server
           ghcid
         ]);
   }
