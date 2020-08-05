274b8ef505cdb5b80f073e106b1bbe24f95e6fb7
Sridhar Ratnakumar <srid@srid.ca>
2020-08-04 21:43:55 -0400

    WIP

--- default.nix
+++ default.nix
@@ -4,13 +4,9 @@ let
     sha256 = "1r4da3kyghqq7vzya5yb295jpnha1fgp755bi6wda4fmhyaynnsp";
   };
   pkgs = import nixpkgsSrc {};
-  gitignoreSrc = pkgs.fetchFromGitHub { 
-    owner = "hercules-ci";
-    repo = "gitignore";
-    # put the latest commit sha of gitignore Nix library here:
-    rev = "c4662e6";
-    # use what nix suggests in the mismatch message here:
-    sha256 = "sha256:1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
+  gitignoreSrc = builtins.fetchTarball { 
+    url = "https://github.com/hercules-ci/gitignore/archive/c4662e6.tar.gz";
+    sha256 = "1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
   };
   inherit (import gitignoreSrc { inherit (pkgs); }) gitignoreSource;
 in 
