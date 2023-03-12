d79326d7b51080227bbba934eb4b38582f0735ea
Sridhar Ratnakumar <srid@srid.ca>
2023-03-12 19:58:56 -0400

    gitignoresrc is not required (flakes already ignores them)

--- flake.nix
+++ flake.nix
@@ -2,16 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    gitignore.url = "github:hercules-ci/gitignore.nix";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, gitignore, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { self', inputs', pkgs, system, ... }:
         let
-          inherit (gitignore.lib) gitignoreSource;
           overlay = self: super: {
-            jrec = self.callCabal2nix "jrec" (gitignoreSource ./.) { };
+            jrec = self.callCabal2nix "jrec" ./. { };
           };
           haskellPackages' = pkgs.haskellPackages.extend overlay;
         in
